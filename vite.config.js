884fd4aae16ddc27ecd229cdb18ffca44585ba7c
bandaman <101086673+Francissha@users.noreply.github.com>
2025-02-23 12:04:42 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Allows imports like "@/components/..."
+    }
+  },
+  server: {
+    port: 3000, // Change port if needed
+  }
+});
